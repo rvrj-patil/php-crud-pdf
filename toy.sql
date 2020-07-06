b3629500b6cce49939bfcd2c5ba2feb0c0281f7d
rvrj-patil <67901386+rvrj-patil@users.noreply.github.com>
2020-07-06 16:05:41 +0530

    Update toy.sql

--- toy.sql
+++ toy.sql
@@ -3,17 +3,10 @@ CREATE TABLE IF NOT EXISTS `toy` (
   `name` varchar(255) NOT NULL,
   `code` varchar(20) NOT NULL,
   `category` varchar(255) NOT NULL,
-  `price` double NOT NULL,
-  `stock_count` bigint(16) NOT NULL,
   PRIMARY KEY (`id`)
 ) 
 
 INSERT INTO `toy` (`id`, `name`, `code`, `category`, `price`, `stock_count`) VALUES
-(9, 'Music Aeroplane', 'TOY#MA01', 'Music Toys', 250, 500),
-(10, 'Power Ranger', 'TOY#BT01', 'Battery Toys', 1000, 100),
-(11, 'Remote Car', 'TOY#RMT01', 'Remote Control Toys', 280, 800),
-(12, 'Bubbles', 'TOY#WT01', 'Water Games', 100, 1000),
-(13, 'Cricket Cards', 'TOY#CD01', 'Card Games', 200, 80),
-(14, 'Basket Ball', 'TOY#BB01', 'Outdoor Toys', 2000, 500),
-(15, 'Word Puzzles', 'TOY#PZ01', 'Puzzles', 200, 200),
-(16, 'Water Gun', 'TOY#WG01', 'Water Games', 100, 1000);
+(1, 'Music Aeroplane', 'TOY#MA01', 'Music Toys'),
+(2, 'Power Ranger', 'TOY#BT01', 'Battery Toys'),
+(3, 'Water Gun', 'TOY#WG01', 'Water Games');
